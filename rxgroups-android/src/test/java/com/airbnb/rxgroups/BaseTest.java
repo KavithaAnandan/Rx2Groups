6a60db6fded1242285c8f5696f6963c4916be04a
Rahul Raja <rahul.110392@gmail.com>
2017-06-25 16:34:53 +0530

    converted RxGroupAndroid test into RxJava2

--- rxgroups-android/src/test/java/com/airbnb/rxgroups/BaseTest.java
+++ rxgroups-android/src/test/java/com/airbnb/rxgroups/BaseTest.java
@@ -3,13 +3,25 @@ package com.airbnb.rxgroups;
 import org.junit.BeforeClass;
 import org.robolectric.shadows.ShadowLog;
 
-import rx.android.plugins.RxAndroidPlugins;
+import java.util.concurrent.Callable;
+
+import io.reactivex.Scheduler;
+import io.reactivex.android.plugins.RxAndroidPlugins;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
+
 
 public abstract class BaseTest {
-  @BeforeClass public static void classSetUp() {
-    ShadowLog.stream = System.out;
-    RxAndroidPlugins instance = RxAndroidPlugins.getInstance();
-    instance.reset();
-    instance.registerSchedulersHook(new TestRxAndroidSchedulersHook());
-  }
+    @BeforeClass
+    public static void classSetUp() {
+        ShadowLog.stream = System.out;
+        RxAndroidPlugins.reset();
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler(new Function<Callable<Scheduler>, Scheduler>() {
+            @Override
+            public Scheduler apply(@NonNull Callable<Scheduler> schedulerCallable) throws Exception {
+                return Schedulers.trampoline();
+            }
+        });
+    }
 }
--- rxgroups-android/src/test/java/com/airbnb/rxgroups/GroupLifecycleManagerTest.java
+++ rxgroups-android/src/test/java/com/airbnb/rxgroups/GroupLifecycleManagerTest.java
@@ -6,104 +6,115 @@ import android.os.Build;
 import com.airbnb.rxgroups.LifecycleResubscription.ObserverInfo;
 import com.airbnb.rxgroups.android.BuildConfig;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 import java.util.concurrent.TimeUnit;
 
 
 import io.reactivex.Observable;
-import io.reactivex.schedulers.Schedulers;
 import io.reactivex.schedulers.TestScheduler;
+import io.reactivex.subjects.PublishSubject;
 import io.reactivex.subscribers.TestSubscriber;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @Config(sdk = Build.VERSION_CODES.LOLLIPOP, constants = BuildConfig.class)
-@RunWith(RobolectricGradleTestRunner.class)
+@RunWith(RobolectricTestRunner.class)
 public class GroupLifecycleManagerTest extends BaseTest {
-  private final LifecycleResubscription resubscription = mock(LifecycleResubscription.class);
-  private final TestScheduler scheduler = Schedulers.test();
-  private final TestSubject<Object> testSubject = TestSubject.create(scheduler);
-  private final TestSubscriber<String> testSubscriber = new TestSubscriber<>();
-  private final String tag = "test";
-  private final ObserverInfo observerInfo = new ObserverInfo(tag, testSubscriber);
-  private final ObservableManager observableManager = mock(ObservableManager.class);
-  private final ObservableGroup group = mock(ObservableGroup.class);
-  private final Object target = new Object();
+    private final LifecycleResubscription resubscription = mock(LifecycleResubscription.class);
+    private final TestScheduler scheduler = new TestScheduler();
+    private final PublishSubject<Object> testSubject = PublishSubject.create();
+    private final TestObserver<String> testObserver = new TestObserver<>();
+    private final String tag = "test";
+    private final ObserverInfo observerInfo = new ObserverInfo(tag, testObserver);
+    private final ObservableManager observableManager = mock(ObservableManager.class);
+    private final ObservableGroup group = mock(ObservableGroup.class);
+    private final Object target = new Object();
 
-  @Test public void testSubscribe() {
-    when(observableManager.newGroup()).thenReturn(group);
-    when(group.hasObservable(tag)).thenReturn(true);
-    when(group.observable(tag)).thenReturn(testSubject);
-    when(resubscription.observers(target)).thenReturn(Observable.just(observerInfo));
+    @Before
+    public void setUp() {
+        testSubject.observeOn(scheduler);
+    }
 
-    GroupLifecycleManager.onCreate(observableManager, resubscription).subscribe(target);
+    @Test
+    public void testSubscribe() {
+        when(observableManager.newGroup()).thenReturn(group);
+        when(group.hasObservable(tag)).thenReturn(true);
+        when(group.observable(tag)).thenReturn(testSubject);
+        when(resubscription.observers(target)).thenReturn(Observable.just(observerInfo));
 
-    assertThat(testSubject.hasObservers()).isTrue();
+        GroupLifecycleManager.onCreate(observableManager, resubscription).subscribe(target);
 
-    testSubject.onNext("hello");
-    testSubject.onCompleted();
-    scheduler.triggerActions();
+        assertThat(testSubject.hasObservers()).isTrue();
 
-    testSubscriber.awaitTerminalEvent(3, TimeUnit.SECONDS);
-    testSubscriber.assertCompleted();
-    testSubscriber.assertValue("hello");
-  }
+        testSubject.onNext("hello");
+        testSubject.onComplete();
+        scheduler.triggerActions();
 
-  @Test public void testSubscribeNoObservables() {
-    when(observableManager.newGroup()).thenReturn(group);
-    when(group.hasObservable(tag)).thenReturn(false);
-    when(group.observable(tag)).thenReturn(testSubject);
-    when(resubscription.observers(target)).thenReturn(Observable.just(observerInfo));
+        testObserver.awaitTerminalEvent(3, TimeUnit.SECONDS);
+        testObserver.assertCompleted();
+        testObserver.assertValue("hello");
+    }
 
-    GroupLifecycleManager.onCreate(observableManager, resubscription).subscribe(target);
+    @Test
+    public void testSubscribeNoObservables() {
+        when(observableManager.newGroup()).thenReturn(group);
+        when(group.hasObservable(tag)).thenReturn(false);
+        when(group.observable(tag)).thenReturn(testSubject);
+        when(resubscription.observers(target)).thenReturn(Observable.just(observerInfo));
 
-    assertThat(testSubject.hasObservers()).isFalse();
+        GroupLifecycleManager.onCreate(observableManager, resubscription).subscribe(target);
 
-    testSubject.onNext("hello");
-    testSubject.onCompleted();
-    scheduler.triggerActions();
+        assertThat(testSubject.hasObservers()).isFalse();
 
-    testSubscriber.awaitTerminalEvent(3, TimeUnit.SECONDS);
-    testSubscriber.assertNotCompleted();
-    testSubscriber.assertNoValues();
-  }
+        testSubject.onNext("hello");
+        testSubject.onComplete();
+        scheduler.triggerActions();
 
-  @Test public void testSubscribeNoObservers() {
-    when(observableManager.newGroup()).thenReturn(group);
-    when(group.hasObservable(tag)).thenReturn(true);
-    when(group.observable(tag)).thenReturn(testSubject);
-    when(resubscription.observers(target)).thenReturn(Observable.<ObserverInfo>empty());
+        testObserver.awaitTerminalEvent(3, TimeUnit.SECONDS);
+        testObserver.assertNotCompleted();
+        testObserver.assertNoValues();
+    }
 
-    GroupLifecycleManager.onCreate(observableManager, resubscription).subscribe(target);
+    @Test
+    public void testSubscribeNoObservers() {
+        when(observableManager.newGroup()).thenReturn(group);
+        when(group.hasObservable(tag)).thenReturn(true);
+        when(group.observable(tag)).thenReturn(testSubject);
+        when(resubscription.observers(target)).thenReturn(Observable.<ObserverInfo>empty());
 
-    assertThat(testSubject.hasObservers()).isFalse();
+        GroupLifecycleManager.onCreate(observableManager, resubscription).subscribe(target);
 
-    testSubject.onNext("hello");
-    testSubject.onCompleted();
-    scheduler.triggerActions();
-  }
+        assertThat(testSubject.hasObservers()).isFalse();
 
-  @Test public void testDestroyFinishingActivity() {
-    when(observableManager.newGroup()).thenReturn(group);
-    when(group.hasObservable(tag)).thenReturn(true);
-    when(group.observable(tag)).thenReturn(testSubject);
-    when(resubscription.observers(target)).thenReturn(Observable.just(observerInfo));
+        testSubject.onNext("hello");
+        testSubject.onComplete();
+        scheduler.triggerActions();
+    }
 
-    GroupLifecycleManager lifecycleManager =
-        GroupLifecycleManager.onCreate(observableManager, resubscription);
-    lifecycleManager.subscribe(target);
+    @Test
+    public void testDestroyFinishingActivity() {
+        when(observableManager.newGroup()).thenReturn(group);
+        when(group.hasObservable(tag)).thenReturn(true);
+        when(group.observable(tag)).thenReturn(testSubject);
+        when(resubscription.observers(target)).thenReturn(Observable.just(observerInfo));
 
-    Activity activity = mock(Activity.class);
-    when(activity.isFinishing()).thenReturn(true);
+        GroupLifecycleManager lifecycleManager =
+                GroupLifecycleManager.onCreate(observableManager, resubscription);
+        lifecycleManager.subscribe(target);
 
-    lifecycleManager.onDestroy(activity);
+        Activity activity = mock(Activity.class);
+        when(activity.isFinishing()).thenReturn(true);
 
-    verify(observableManager).destroy(group);
-  }
+        lifecycleManager.onDestroy(activity);
+
+        verify(observableManager).destroy(group);
+    }
 }
